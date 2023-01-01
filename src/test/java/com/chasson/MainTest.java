8cb37db45e29bd88710455fefe578534d9708968
Connor Hasson <cohasson@yahoo.com>
2023-01-01 14:25:13 -0500

    init

---
+++ src/test/java/com/chasson/MainTest.java
@@ -0,0 +1,13 @@
+package com.chasson;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
